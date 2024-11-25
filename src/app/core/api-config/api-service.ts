99fd26ab818de99c72304186857f0ff88f4e975e
Gago <egagik69@gmail.com>
2024-11-25 22:14:47 +0400

    create schedule json and functionality

--- src/app/core/api-config/api-service.ts
+++ src/app/core/api-config/api-service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
 import { HomePageDate } from "../types/homePage.interface";
+import { HomeSchedule} from "../types/schedule.interface";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 
 
@@ -17,5 +19,35 @@ export class ApiService{
           return res.Home.days
         }))
     }
+
+    public getSchedule(day="Monday"){
+        return this.http.get<HomeSchedule>('json/schedule.json')
+        .pipe(map((res:HomeSchedule)=>{
+            console.log('res',res.schedule.find((lesson)=>lesson.day === day))
+            return res.schedule.find((lesson)=>lesson.day === day)
+          }))
+
+        // return   toSignal(
+        //     this.http.get<HomeSchedule>('json/schedule.json').pipe(
+        //       map((res: HomeSchedule) => {
+        //         const lesson = res.schedule.find((lesson) => lesson.day === day);
+        //         console.log('Lesson:', lesson);
+        //         return lesson;
+        //       })
+        //     ),
+        //     { initialValue: null }
+        // );
+    }
+
+    public schedule = toSignal(
+        this.http.get<HomeSchedule>('json/schedule.json').pipe(
+          map((res: HomeSchedule) => {
+            const lesson = res.schedule.find((lesson) => lesson.day === "Monday");
+            console.log('Lesson:', lesson);
+            return lesson;
+          })
+        ),
+        { initialValue: null }
+    );
     
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, EnvironmentInjector, inject, runInInjectionContext, signal} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ApiService } from '../../core/api-config/api-service';
+import { Schedule } from '../../core/types/schedule.interface';
 
 @Component({
   selector: 'visp-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent{
+
+  private environmentInjector = inject(EnvironmentInjector);
   private apiService = inject(ApiService);
   public weekday = toSignal(this.apiService.getWeekday());
+  public schedule = toSignal( this.apiService.getSchedule())
 
+  public getSchedule(ev:Event){
+    const day:string = (ev.target as HTMLSelectElement).value; 
+    // this.schedule = toSignal( this.apiService.getSchedule(day))
+    runInInjectionContext(this.environmentInjector, () => {
+      this.apiService = inject(ApiService);
+      this.schedule = toSignal( this.apiService.getSchedule(day))
+    });
+  }
 }
 
 
